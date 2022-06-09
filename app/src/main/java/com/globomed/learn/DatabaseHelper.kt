5f7b8b12c57a0500174b83a1de99957498ceea5e
Thamsanqa Shabalala <thammy202@gmail.com>
2022-06-09 10:14:26 +0200

    Insert employee details into database.

--- app/src/main/java/com/globomed/learn/DatabaseHelper.kt
+++ app/src/main/java/com/globomed/learn/DatabaseHelper.kt
@@ -5,16 +5,17 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHelper(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
     override fun onCreate(db: SQLiteDatabase?) {
-      db?.execSQL(GloboMedDbContract.EmployeeEntry.SQL_CREATE_ENTRIES)
+        db?.execSQL(GloboMedDbContract.EmployeeEntry.SQL_CREATE_ENTRIES)
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         db?.execSQL(GloboMedDbContract.EmployeeEntry.SQL_DROP_TABLE)
         onCreate(db)
     }
 
-    companion object{
+    companion object {
         const val DATABASE_NAME = "globomed.db"
         const val DATABASE_VERSION = 1
     }
--- app/src/main/java/com/globomed/learn/GloboMedDBContract.kt
+++ app/src/main/java/com/globomed/learn/GloboMedDBContract.kt
@@ -5,24 +5,19 @@ import android.provider.BaseColumns._ID
 
 object GloboMedDbContract {
 
-
-
     object EmployeeEntry : BaseColumns {
         const val TABLE_NAME = "employee"
         const val COLUMN_NAME = "name"
         const val COLUMN_DOB = "dob"
         const val COLUMN_DESIGNATION = "designation"
 
-
-
-        const val SQL_CREATE_ENTRIES: String =
-            "CREATE TABLE $TABLE_NAME} (" +
+        const val SQL_CREATE_ENTRIES =
+            "CREATE TABLE $TABLE_NAME (" +
                     _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    "$COLUMN_NAME} TEXT NOT NULL, " +
-                    "$COLUMN_DOB INTEGER  NOT NULL, " +
-                    "$COLUMN_DESIGNATION} TEXT NOT NULL)"
+                    "$COLUMN_NAME TEXT NOT NULL, " +
+                    "$COLUMN_DOB INTEGER NOT NULL, " +
+                    "$COLUMN_DESIGNATION TEXT NOT NULL)"
 
-        const val SQL_DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME}"
-
+        const val SQL_DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
     }
 }
